a8ad90bd27ce217f1200d5a8b4f1d7971ac2a0e6
Maarten Engels <maartene@thedreamweb.eu>
2026-02-22 12:40:09 +0100

    R: Contract-test: possibility to add more players

--- api/contract/ContractTest/Sources/ContractTest/ContractTest.swift
+++ api/contract/ContractTest/Sources/ContractTest/ContractTest.swift
@@ -3,8 +3,6 @@ import LCLWebSocket
 import NIO
 import YouSunkMyBattleshipCommon
 
-let player = Player(id: "Player_1")
-
 final class Box<T: Sendable>: @unchecked Sendable {
     private(set) var value: T
 
@@ -17,38 +15,41 @@ final class Box<T: Sendable>: @unchecked Sendable {
     }
 }
 
-final class ContractTest: Sendable {
-    let config: LCLWebSocket.Configuration
+@MainActor
+final class GamePlayer {
+    enum PlayerState {
+        case idle
+        case connecting
+        case connected
+        case creatingGame
+        case playing
+        case finished
+    }
     let encoder = JSONEncoder()
     let decoder = JSONDecoder()
+    let currentGameState = Box<GameState?>(value: nil)
+    let config: LCLWebSocket.Configuration
     let hostname: String
     let port: String
-    
+    let player: Player
+    var state: PlayerState = .idle
+    var client: WebSocketClient?
+    let websocket = Box<WebSocket?>(value: nil)
+    let locked = Box(value: false)
+    var availableMoves = Set<Coordinate>()
 
-    init(hostname: String = "127.0.0.1", port: String = "8080") {
+    
+    private func setState(_ newState: PlayerState) {
+        self.state = newState
+    }
+    
+    init(playerID: String, config: LCLWebSocket.Configuration, hostname: String, port: String) {
+        self.config = config
         self.hostname = hostname
         self.port = port
-        config = LCLWebSocket.Configuration(
-            maxFrameSize: 1 << 16,
-            autoPingConfiguration: .enabled(pingInterval: .seconds(4), pingTimeout: .seconds(10)),
-            leftoverBytesStrategy: .forwardBytes
-        )
-    }
-
-    func run() async throws {
-        var availableMoves = Set<Coordinate>()
-
-        let currentGameState = Box(value: GameState(currentPlayer: player))
-        let websocket = Box<WebSocket?>(value: nil)
-        let locked = Box(value: false)
-
-        for y in 0..<Board.rows.count {
-            for x in 0..<Board.columns.count {
-                availableMoves.insert(Coordinate(x: x, y: y))
-            }
-        }
-
+        self.player = Player(id: playerID)
         var client = LCLWebSocket.client()
+        
         client.onOpen { ws in
             let localBoard = Board.makeFilledBoard()
             let placedShips = localBoard.placedShips.map { $0.toDTO() }
@@ -57,14 +58,16 @@ final class ContractTest: Sendable {
             try? ws.send(
                 createBoardCommand.toByteBuffer(using: self.encoder), opcode: .binary, promise: nil
             )
-            websocket.set(ws)
+            self.websocket.set(ws)
+            Task {
+                await self.setState(.connected)
+            }
         }
 
         client.onBinary { websocket, binary in
             if let state = try? self.decoder.decode(GameState.self, from: binary) {
-                currentGameState.set(state)
-                print(currentGameState.value)
-                locked.set(false)
+                self.currentGameState.set(state)
+                self.locked.set(false)
             }
         }
 
@@ -75,55 +78,123 @@ final class ContractTest: Sendable {
         client.onError { error in
             fatalError("Received error: \(error)")
         }
-
-        _ = client.connect(to: "ws://\(hostname):\(port)/game/\(player.id)", configuration: config)
-        try await Task.sleep(nanoseconds: 1_000_000_000)
-        let deadline = Date().addingTimeInterval(300)
-        while currentGameState.value.state != .finished {
-            guard Date() < deadline else {
-                fatalError("Out of time")
-            }
-
-            try await Task.sleep(nanoseconds: 100_000_000)
-            if locked.value == false, currentGameState.value.state == .play,
-                currentGameState.value.currentPlayer == player
-            {
-
-                locked.set(true)
-
-                if let move = availableMoves.randomElement() {
-                    availableMoves.remove(move)
-
-                    let fireCommand = GameCommand.fireAt(coordinate: move)
-                    try websocket.value?.send(
-                        fireCommand.toByteBuffer(using: encoder), opcode: .binary, promise: nil)
-                } else {
-                    print("No more moves to make")
-                }
+        
+        self.client = client
+        
+        for y in 0..<Board.rows.count {
+            for x in 0..<Board.columns.count {
+                availableMoves.insert(Coordinate(x: x, y: y))
             }
         }
+    }
+    
+    func act() async throws {
+        switch state {
+        case .idle:
+            connect()
+        case .connecting:
+            break
+        case .connected:
+            try createGame()
+        case .creatingGame:
+            checkCreatedGame()
+        case .playing:
+            try tryPlay()
+        case .finished:
+            break
+        }
+    }
+    
+    func connect() {
+        _ = client?.connect(to: "ws://\(hostname):\(port)/game/\(player.id)", configuration: config)
+        state = .connecting
+    }
+    
+    func createGame() throws {
+        let board = Board.makeFilledBoard()
+        let placedShips = board.placedShips.map { $0.toDTO() }
+        
+        let createGameCommand = GameCommand.createGame(placedShips: placedShips, speed: .fast)
+        try websocket.value?.send(
+            createGameCommand.toByteBuffer(using: encoder), opcode: .binary, promise: nil)
+        state = .creatingGame
+    }
+    
+    func checkCreatedGame() {
+        if currentGameState.value != nil {
+            state = .playing
+        }
+    }
+    
+    func tryPlay() throws {
+        guard let gameState = currentGameState.value else {
+            return
+        }
+        
+        guard gameState.state == .play else {
+            print("Game finished!")
+            state = .finished
+            return
+        }
+        
+        guard gameState.currentPlayer == player else {
+            return
+        }
+        
+        guard locked.value == false else {
+            return
+        }
+        
+        locked.set(true)
+        
+        if let move = availableMoves.randomElement() {
+            availableMoves.remove(move)
 
+            let fireCommand = GameCommand.fireAt(coordinate: move)
+            try websocket.value?.send(
+                fireCommand.toByteBuffer(using: encoder), opcode: .binary, promise: nil)
+        } else {
+            print("No more moves to make")
+        }
+    }
+}
+
+@MainActor
+final class ContractTest: Sendable {
+    let player1: GamePlayer
+    
+    init(hostname: String = "127.0.0.1", port: String = "8080") {
+        let config = LCLWebSocket.Configuration(
+            maxFrameSize: 1 << 16,
+            autoPingConfiguration: .enabled(pingInterval: .seconds(4), pingTimeout: .seconds(10)),
+            leftoverBytesStrategy: .forwardBytes
+        )
+        self.player1 = GamePlayer(playerID: "Player_1", config: config, hostname: hostname, port: port)
+    }
+
+    func run() async throws {
+        while player1.state != .finished {
+            try await player1.act()
+            print(gameStateToString(player1: player1))
+            try await Task.sleep(nanoseconds: 100_000_000)
+        }
         print("Game finished")
-    }
-}
 
-extension Encodable {
-    func toByteBuffer(using encoder: JSONEncoder) throws -> ByteBuffer {
-        let data = try encoder.encode(self)
-        return ByteBuffer(data: data)
     }
-}
-
-extension GameState: @retroactive CustomStringConvertible {
-    public var description: String {
-        var result = "State: \(state)\n"
-        result += "Current player: \(currentPlayer)\n"
-        result += "Ships to destroy: \(shipsToDestroy)\n"
+    
+    func gameStateToString(player1: GamePlayer) -> String {
+        guard let gameState = player1.currentGameState.value else {
+            return "Gamestate not yet initialized"
+        }
+        
+        var result = "State: \(gameState.state)\n"
+        result += "Current player: \(gameState.currentPlayer)\n"
+        result += "Ships to destroy: \(gameState.shipsToDestroy)\n"
 
         result += "Player 1             Player 2\n"
 
-        let player1Cells = cells[player]!
-        let player2Cells = cells.first { $0.key != player }!.value
+        let player1Cells = gameState.cells[player1.player]!
+        let player2Cells = gameState.cells.first { $0.key != player1.player }!.value
 
         for y in 0..<Board.rows.count {
             var line = ""
@@ -141,8 +212,15 @@ extension GameState: @retroactive CustomStringConvertible {
 
             result += line + "\n"
         }
-        result += "\(lastMessage)\n"
-
+        result += "\(gameState.lastMessage)\n"
         return result
+    }
+
+}
+
+extension Encodable {
+    func toByteBuffer(using encoder: JSONEncoder) throws -> ByteBuffer {
+        let data = try encoder.encode(self)
+        return ByteBuffer(data: data)
     }
 }
