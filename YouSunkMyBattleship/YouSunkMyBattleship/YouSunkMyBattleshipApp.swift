11de853c24740b8ec7af8f9e4879355b52f2e5b9
Maarten Engels <maartene@mac.com>
2026-02-24 14:05:02 +0100

    FE: progress with 2 player games

--- YouSunkMyBattleship/YouSunkMyBattleship/YouSunkMyBattleshipApp.swift
+++ YouSunkMyBattleship/YouSunkMyBattleship/YouSunkMyBattleshipApp.swift
@@ -10,12 +10,20 @@ import WSDataProvider
 import Foundation
 import YouSunkMyBattleshipCommon
 
+let player = {
+   if let playerID = UserDefaults.standard.string(forKey: "playerID") {
+        return Player(id: playerID)
+   } else {
+       let playerID = "player_\(Int.random(in: 0 ... 1_000_000))"
+       UserDefaults.standard.set(playerID, forKey: "playerID")
+       return Player(id: playerID)
+   }
+}()
+
 #if targetEnvironment(simulator)
-let player = Player(id: "simulator_player")
 let wsURL = URL(string: "ws://localhost:8080/game/\(player.id)")!
 let httpURL = URL(string: "http://localhost:8080/games")!
 #else
-let player = Player(id: "device_player")
 let wsURL = URL(string: "wss://service-ykxo8.ondigitalocean.app/game/\(player.id)")!
 let httpURL = URL(string: "https://service-ykxo8.ondigitalocean.app/games")!
 #endif
