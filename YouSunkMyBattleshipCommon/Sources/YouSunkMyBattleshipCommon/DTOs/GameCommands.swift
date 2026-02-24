d406760659f29d7989baa0bf29aff84836d8dd64
Maarten Engels <maartene@thedreamweb.eu>
2026-02-24 11:49:25 +0100

    R: GameCommand uses auto synthesised Equatable

--- YouSunkMyBattleshipCommon/Sources/YouSunkMyBattleshipCommon/DTOs/GameCommands.swift
+++ YouSunkMyBattleshipCommon/Sources/YouSunkMyBattleshipCommon/DTOs/GameCommands.swift
@@ -5,21 +5,7 @@ public enum GameCommand: Codable {
     case load(gameID: String)
 }
 
-extension GameCommand: Equatable {
-    public static func == (lhs: GameCommand, rhs: GameCommand) -> Bool {
-        if case .fireAt(let lhsCoordinate) = lhs, case .fireAt(let rhsCoordinate) = rhs {
-            return lhsCoordinate == rhsCoordinate
-        } else if case .load(let lhsGameID) = lhs, case .load(let rhsGameID) = rhs {
-            return lhsGameID == rhsGameID
-        } else if case .placeShip(let lhsCoordinates) = lhs, case .placeShip(let rhsCoordinates) = rhs {
-            return lhsCoordinates == rhsCoordinates
-        } else if case .createGame(let lhsWithCPU, let lhsSpeed) = lhs, case .createGame(let rhsWithCPU, let rhsSpeed) = rhs {
-            return lhsWithCPU == rhsWithCPU && lhsSpeed == rhsSpeed
-        } else {
-            return false
-        }
-    }
-}
+extension GameCommand: Equatable { }
 
 public enum GameSpeed: Codable {
     case fast
